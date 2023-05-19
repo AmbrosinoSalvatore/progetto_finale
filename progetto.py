586fbc6b798e9848768f8515145b3e5adeb6a68c
GabrieleGalvani <dilecce.gabriele@iisgalvanimi.edu.it>
2023-05-19 10:59:49 +0200

    .

--- progetto.py
+++ progetto.py
@@ -1,22 +1,57 @@
 import tkinter as tk
+import pymssql
 
-def saluta():
-    nome = entry_nome.get()
-    label_saluto.config(text="Ciao " + nome)
+def register_user():
+    # PRENDEi dati inseriti dall'utente
+    username = username_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    confirm_password = confirm_password_entry.get()
 
-root = tk.Tk()
+    # connessione al database
+    conn = pymssql.connect(server='192.168.40.16\SQLEXPRESS', user='dilecce.gabriele', password='xxx123##', database='dilecce.gabriele')
 
-label_nome = tk.Label(root, text="Inserisci il tuo nome:")
-label_nome.pack()
+    # creazione del cursore
+    cursor = conn.cursor()
 
-entry_nome = tk.Entry(root)
-entry_nome.pack()
+    # inserimento dei dati degli utenti nella tabella
+    cursor.execute('INSERT INTO Utenti (Username, Email, Password, ConfermaPassword) VALUES (%s,%s,%s,%s)',
+                   (username, email, password, confirm_password))
+    conn.commit()
 
-button_saluta = tk.Button(root, text="Saluta", command=saluta)
-button_saluta.pack()
+    # chiusura della connessione
+    conn.close()
 
-label_saluto = tk.Label(root)
-label_saluto.pack()
+    # messaggio di conferma
+    confirmation_label.config(text="Registrazione completata con successo.")
 
-s
-root.mainloop()
+# creazione della finestra principale
+window = tk.Tk()
+window.title("Registrazione Utente")
+
+# creazione dei widget della finestra
+username_label = tk.Label(text="Username:")
+username_entry = tk.Entry()
+email_label = tk.Label(text="Email:")
+email_entry = tk.Entry()
+password_label = tk.Label(text="Password:")
+password_entry = tk.Entry(show="*")
+confirm_password_label = tk.Label(text="Conferma Password:")
+confirm_password_entry = tk.Entry(show="*")
+register_button = tk.Button(text="Registrati", command=register_user)
+confirmation_label = tk.Label(text="")
+
+# posizionamento dei widget nella finestra
+username_label.pack()
+username_entry.pack()
+email_label.pack()
+email_entry.pack()
+password_label.pack()
+password_entry.pack()
+confirm_password_label.pack()
+confirm_password_entry.pack()
+register_button.pack()
+confirmation_label.pack()
+
+# avvio dell'applicazione
+window.mainloop()
